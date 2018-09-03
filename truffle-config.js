3ed060af15f48b07bddcd6c95d58753d7801919e
zutobg <zytobg@gmail.com>
2018-09-03 11:51:37 +0300

    comment mainNet & privateNet from the config in order to run tests on dev

--- truffle-config.js
+++ truffle-config.js
@@ -3,13 +3,13 @@ const web3 = new Web3();
 const WalletProvider = require("truffle-wallet-provider");
 const Wallet = require('ethereumjs-wallet');
 
-var privateNetPrivateKey = new Buffer("", "hex")
-var privateNetWallet = Wallet.fromPrivateKey(privateNetPrivateKey);
-var privateNetProvider = new WalletProvider(privateNetWallet, "http://10.128.1.248:8545");
+// var privateNetPrivateKey = new Buffer("", "hex")
+// var privateNetWallet = Wallet.fromPrivateKey(privateNetPrivateKey);
+// var privateNetProvider = new WalletProvider(privateNetWallet, "http://10.128.1.248:8545");
 
-var mainNetPrivateKey = new Buffer("", "hex")
-var mainNetWallet = Wallet.fromPrivateKey(mainNetPrivateKey);
-var mainNetProvider = new WalletProvider(mainNetWallet, "https://mainnet.infura.io/");
+// var mainNetPrivateKey = new Buffer("", "hex")
+// var mainNetWallet = Wallet.fromPrivateKey(mainNetPrivateKey);
+// var mainNetProvider = new WalletProvider(mainNetWallet, "https://mainnet.infura.io/");
 
 module.exports = {
   networks: {
@@ -23,18 +23,18 @@ module.exports = {
       port: 8545,
       network_id: "*"
     },
-    privatenet: {
-      provider: privateNetProvider,
-      gas: 4600000,
-      gasPrice: web3.toWei("30", "gwei"),
-      network_id: "15"
-    },
-    mainnet: {
-      provider: mainNetProvider,
-      gas: 7984340,
-      gasPrice: web3.toWei("30", "gwei"),
-      network_id: "1"
-    },
+    // privatenet: {
+    //   provider: privateNetProvider,
+    //   gas: 4600000,
+    //   gasPrice: web3.toWei("30", "gwei"),
+    //   network_id: "15"
+    // },
+    // mainnet: {
+    //   provider: mainNetProvider,
+    //   gas: 7984340,
+    //   gasPrice: web3.toWei("30", "gwei"),
+    //   network_id: "1"
+    // },
     coverage: {
       host: "localhost",
       network_id: "*",
